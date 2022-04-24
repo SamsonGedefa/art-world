b4c79863dfd2cc800255da1b23749db9198dd384
Rec2 <39541952+rec2@users.noreply.github.com>
2022-04-24 18:18:37 -0500

    add login/logut button

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,16 +7,55 @@ import { modalState } from "../atoms/modalAtom";
 import { RiMenu4Fill } from "react-icons/ri";
 import { IoMdAdd } from "react-icons/io";
 import { navMenuState } from "atoms/navMenuAtom";
-import Avatar from "./Avatar";
+import SearchBar from "./SearchBar";
+import { useSession, signIn, signOut } from "next-auth/react"
+
+
+
 export default function Nav() {
   const [modalOpen, setModalOpen] = useRecoilState(modalState);
   const [menuOpen, setMenuOpen] = useRecoilState(navMenuState);
+  const { data: session } = useSession()
 
   const handleMenu = () => {
     menuOpen ? setMenuOpen(false) : setMenuOpen(true);
   };
-  return (
-    <header className="sticky top-0 overflow-hidden h-14 flex justify-start items-center font-semibold uppercase z-10 bg-[#06070D] text-white shadow-md shadow-gray-800/40">
+  if (session) {
+    return (
+      <header className="sticky top-0 overflow-hidden h-14 flex justify-start items-center font-semibold uppercase z-10 bg-[#06070D] text-white shadow-md shadow-gray-800/40">
+        <div
+          onClick={handleMenu}
+          className="cursor-pointer hover:text-[#85f8c6] ml-7"
+        >
+          <RiMenu4Fill size={30} className="" />
+        </div>
+        <div className="relative flex items-center justify-center w-[140px] h-[140px] cursor-pointer">
+          <Link href={"/"}>
+            <a>
+              <Image src="/logo.png" layout="fill" objectFit="cover" />
+            </a>
+          </Link>
+        </div>
+        <div><SearchBar/></div>
+        <>
+        welcome  {session.user.username}!<br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="flex justify-center mt-auto ml-auto mb-auto mr-5 items-center space-x-2 bg-[#11EE8F] text-white rounded-sm w-36 h-[36px] text-lg font-bold shadow-md hover:bg-[#a5ddc5]"
+          onClick={() => setModalOpen(true)}
+        >
+          <IoMdAdd size={20} className="" />
+
+          <span className="inline">Post</span>
+        </motion.button>
+      </header>
+    );
+  } else {
+    return (
+      <header className="sticky top-0 overflow-hidden h-14 flex justify-start items-center font-semibold uppercase z-10 bg-[#06070D] text-white shadow-md shadow-gray-800/40">
       <div
         onClick={handleMenu}
         className="cursor-pointer hover:text-[#85f8c6] ml-7"
@@ -30,11 +69,15 @@ export default function Nav() {
           </a>
         </Link>
       </div>
-
+      <div><SearchBar/></div>
+      <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+      </>
       <motion.button
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
-        className="flex justify-center mt-auto ml-auto mb-auto mr-5  items-center space-x-2 bg-[#11EE8F] text-white rounded-sm w-36 h-[36px] text-lg font-bold shadow-md hover:bg-[#a5ddc5]"
+        className="flex justify-center mt-auto ml-auto mb-auto mr-5 items-center space-x-2 bg-[#11EE8F] text-white rounded-sm w-36 h-[36px] text-lg font-bold shadow-md hover:bg-[#a5ddc5]"
         onClick={() => setModalOpen(true)}
       >
         <IoMdAdd size={20} className="" />
@@ -42,5 +85,6 @@ export default function Nav() {
         <span className="inline">Post</span>
       </motion.button>
     </header>
-  );
+    )
+  }
 }
