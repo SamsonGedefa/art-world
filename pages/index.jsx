d6df1b562dcf4c369946e4e8599c3a45e917baf2
Samson Gedefa <samsongedefa@gmail.com>
2022-04-29 22:20:02 -0500

    fixed error

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,9 @@
 import Head from "next/head";
 import React, { useState, useEffect } from "react";
-// import { connectToDatabase } from "@/lib/middleware/database";
 import { Layout } from "../components/Layout";
 import Feed from "@/components/Feed";
-import { usePostPages } from "@/lib/post";
 import { getSession } from "next-auth/react";
 import { findUserByUsername } from "@/lib/db/user";
-import { useRecoilState } from "recoil";
-import { likeUnlikeState } from "@/atoms/likeAtom";
-import { useLocalStorage } from "@/lib/useLike";
-import { useInterpret } from "@xstate/react";
-import { likeMachine } from "machines/likeState";
 
 export default function Home({ user }) {
   return (
